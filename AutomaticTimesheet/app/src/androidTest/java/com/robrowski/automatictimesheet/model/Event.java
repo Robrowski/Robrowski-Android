bbc861f824df144b50b151923685a5b544f355b2
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-06-19 20:36:47 -0400

    started edit event view

---
+++ AutomaticTimesheet/app/src/androidTest/java/com/robrowski/automatictimesheet/model/Event.java
@@ -0,0 +1,16 @@
+package com.robrowski.automatictimesheet.model;
+
+import java.util.Calendar;
+
+/**
+ * Created by Robrowski on 6/19/2015.
+ */
+public class Event {
+
+    private Calendar mCalendar; // holds the date and time?
+    private String mLocName;
+    private boolean mEntering;
+
+
+
+}
--- AutomaticTimesheet/app/src/main/java/com/robrowski/automatictimesheet/activity/EditEventActivity.java
+++ AutomaticTimesheet/app/src/main/java/com/robrowski/automatictimesheet/activity/EditEventActivity.java
@@ -1,19 +1,104 @@
 package com.robrowski.automatictimesheet.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.robrowski.automatictimesheet.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class EditEventActivity extends ActionBarActivity {
+    private static final String TAG = EditEventActivity.class.toString();
+
+    private Calendar myCalendar = Calendar.getInstance(); // TODO  Maybe keep this is the "event" object?
+    private TextView mEditDate, mEditTime;
+
+    private DatePickerDialog.OnDateSetListener mOnDateSetListener;
+    private TimePickerDialog.OnTimeSetListener mOnTimeSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_event);
+
+        mEditDate = (TextView) findViewById(R.id.edit_date);
+        mEditTime = (TextView) findViewById(R.id.edit_time);
+        updateDateLabel(); // Assumes event data is already loaded
+        updateTimeLabel();
+
+         mOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDateLabel();
+            }
+        };
+
+        mOnTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay );
+                myCalendar.set(Calendar.MINUTE, minute);
+                updateTimeLabel();
+            }
+        };
+
+
     }
+
+    /** Called when the date field is clicked on */
+    public void editDate(View v){
+        new DatePickerDialog(this,
+                mOnDateSetListener,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    /** Called when the time field is clicked on */
+    public void editTime(View v){
+        Log.i(TAG, "Edit time clicked");
+        new TimePickerDialog(this,
+                mOnTimeSetListener,
+                myCalendar.get(Calendar.HOUR_OF_DAY),
+                myCalendar.get(Calendar.MINUTE),
+                false).show(); // TODO 24 hr mode as a preference
+    }
+    
+    
+    
+    private void updateDateLabel(){
+        String dateFormat = "MMM dd, yyyy"; // TODO better date format
+        // TODO cute stuff to say "Yesterday" or "Two Days ago" or "Today"
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+        mEditDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void updateTimeLabel(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat sdf = new SimpleDateFormat(timeFormat, Locale.US);
+        mEditTime.setText(sdf.format(myCalendar.getTime()));
+    }
+    
+
+
+
+
 
 
     @Override
@@ -37,4 +122,8 @@ public class EditEventActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
